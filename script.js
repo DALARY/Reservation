04a585b2052276f7798873cbaf0309d928de4a47
DALARY <75627368+DALARY@users.noreply.github.com>
2024-01-22 17:20:05 +0100

    Finish

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let form = document.querySelector("form");
+const form = document.querySelector("form");
 const select = document.querySelector(".select");
 const date = [
   "mardi 26 septembre",
@@ -7,42 +7,147 @@ const date = [
   "vendredi 28 octobre",
   "lundi 15 décembre",
 ];
-const submit = document.querySelectorAll(".submit");
 const add = document.querySelector(".add");
-const divAdd = document.querySelector(".divAdd");
+const submit = document.querySelector(".submit");
 const buttonDiv = document.querySelector(".buttonDiv");
 
-// Crée les options en fonction du tableau date
+select.addEventListener("change", () => {
+  if (select.selectedIndex == 0) {
+    add.setAttribute("disabled", "");
+    submit.setAttribute("disabled", "");
+  } else {
+    add.removeAttribute("disabled", "");
+    submit.removeAttribute("disabled", "");
+    add.classList.add("btnAdd");
+    submit.classList.add("btnSubmit");
+  }
+});
+
+/* Crée les options par défaut en fonction du tableau date */
 for (let i = 0; i < date.length; i++) {
-  let option = document.createElement("option");
+  const option = document.createElement("option");
   option.textContent = date[i];
   select.appendChild(option);
 }
 
 let index = 1;
+const selectedDates = [];
+const valueInput = [];
+
+/* Permet de supprimer la réservation */
+form.addEventListener("click", (event) => {
+  const target = event.target;
+
+  if (target.classList.contains("delete")) {
+    // Si le clic a été effectué sur un bouton avec la classe "delete"
+    const newDiv = target.closest(".divAdd");
+
+    if (newDiv) {
+      // Si le bouton "Supprimer" est à l'intérieur d'une nouvelle div
+      const newSelect = newDiv.querySelector(".select");
+      const selectedDateIndex = selectedDates.indexOf(newSelect.value);
+
+      selectedDates.splice(selectedDateIndex, 1);
+      valueInput.splice(selectedDateIndex, 1);
+
+      const restoredDate = newSelect.value;
+
+      const allSelects = document.querySelectorAll(".divAdd .select");
+      allSelects.forEach((select) => {
+        const option = document.createElement("option");
+        option.textContent = restoredDate;
+        select.appendChild(option);
+      });
+
+      add.style.display = "block";
+      newDiv.remove();
+      index--;
+    }
+  }
+});
+
 add.addEventListener("click", () => {
   if (index < date.length) {
-    let newDiv = divAdd.cloneNode(true);
-    form.insertBefore(newDiv, buttonDiv);
+    /* Création des éléments de ma pages HTML */
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("divAdd");
 
-    // Désactive la balise <select> de la div précédente
+    const newSelect = document.createElement("select");
+    newSelect.classList.add("select");
+    newDiv.appendChild(newSelect);
+
+    const defaultOption = document.createElement("option");
+    defaultOption.setAttribute("selected", "");
+    defaultOption.setAttribute("disabled", "");
+    defaultOption.textContent = "Choisir une date";
+    newSelect.appendChild(defaultOption);
+
+    const newInput = document.createElement("input");
+    newInput.setAttribute("type", "number");
+    newInput.setAttribute("value", 1);
+    newInput.setAttribute("min", 1);
+    newDiv.appendChild(newInput);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.setAttribute("type", "button");
+    deleteBtn.textContent = "Supprimer";
+    deleteBtn.classList.add("delete");
+    newDiv.appendChild(deleteBtn);
+
+    form.insertBefore(newDiv, buttonDiv);
+    add.setAttribute("disabled", "");
+    submit.setAttribute("disabled", "");
+    add.classList.remove("btnAdd");
+    submit.classList.remove("btnSubmit");
+
+    /* Modification des options du select */
     if (index > 0) {
-      let previousDiv = form.children[index - 1];
-      let previousSelect = previousDiv.querySelector(".select");
+      const previousDiv = form.children[index - 1];
+      const previousSelect = previousDiv.querySelector(".select");
       previousSelect.setAttribute("disabled", "");
 
-      let selectIndex = date.indexOf(previousSelect.value);
-      console.log(selectIndex);
-      if (selectIndex !== -1) {
-        date.splice(selectIndex, 1);
+      const previousIndex = previousDiv.querySelector("input");
+      previousIndex.setAttribute("disabled", "");
+
+      const previousDelete = previousDiv.querySelector(".delete");
+      previousDelete.style.display = "block";
+
+      /* Ajoute la date et le nombre de place précédemment sélectionnée */
+      selectedDates.push(previousSelect.value);
+      valueInput.push(previousIndex.value);
+
+      /* Filtre les éléments sélectionner en fonction des dates */
+      const availableDates = date.filter(
+        (dateItem) => !selectedDates.includes(dateItem)
+      );
+
+      /* Crée les nouvelles dates disponible */
+      for (let i = 0; i < availableDates.length; i++) {
+        const option = document.createElement("option");
+        option.textContent = availableDates[i];
+        newSelect.appendChild(option);
       }
 
-      // Supprime l'option sélectionnée du nouveau select dans la nouvelle div
-      newDiv.querySelector(".select").remove(selectIndex);
+      newSelect.addEventListener("change", () => {
+        if (newSelect.selectedIndex == 0) {
+          add.setAttribute("disabled", "");
+          submit.setAttribute("disabled", "");
+        } else {
+          add.removeAttribute("disabled", "");
+          submit.removeAttribute("disabled", "");
+          add.classList.add("btnAdd");
+          submit.classList.add("btnSubmit");
+        }
+      });
 
-      console.log(selectIndex);
+      if (index === date.length - 1) {
+        add.style.display = "none";
+      }
     }
+
     newDiv.querySelector(".select").removeAttribute("disabled");
     index++;
+
+    console.log(selectedDates, valueInput);
   }
 });
